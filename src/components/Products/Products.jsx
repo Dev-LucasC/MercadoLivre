63fb80b3627cbfdfb98c94b01d9ef392e67a84f6
Dev-LucasC <lucaconceicao@hotmail.com>
2023-06-16 16:59:09 -0300

    Loading

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,6 +2,7 @@ import React, {useState, useEffect } from 'react';
 import './Products.css';
 import fetchProducts from '../../api/fetchProducts';
 import ProductCard from '../ProductCard/ProductCard';
+import Loading from '../Loading/Loading';
 
 function Products () {
 
@@ -18,7 +19,7 @@ function Products () {
 
   return (
     <section className="products container">
-
+      <Loading />
       {
         products.map((product) => <ProductCard key={product.id} data={product}/>)
       }
